cf00ec6b213a3d1fb2f31484264274c369a72abe
Ali <ali@MacBook-Air.local>
2022-03-30 23:31:37 -0700

    main and make

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "account.h"
+#include "money.h"
 
 using namespace std;
 
 int main(){
     Money starting = Money (300,23);
-    Account myAccount = Account (starting); //i think
+    Account myAccount = Account (starting);
     myAccount.makeDeposit(Money(200,0));
     myAccount.makeDeposit(Money(300,24));
     myAccount.makeDeposit(Money(501,22));
@@ -17,17 +18,13 @@ int main(){
 
     cout<<myAccount<<endl;
 
-    m(300.10,00) //cents
-
-    //constructor for money object -- friend constructor and variable
-
-    //Write code to verify that your relational operators are working correctly. This is up to you, however, you must have printouts to the console showing that the operations are correct.
-
-    /*Money m(100,10);
+    Money m(300,10);
     Money n(100,10);
     if(m > n){
-        // print something
-    } else // etc.*/
+        cout<< m <<endl;
+    }else{
+        cout<<"false"<<endl;
+    }
 
     return 0;
 }
---
+++ makefile
@@ -0,0 +1,14 @@
+hw3: main.o money.o account.o
+	g++ -o lab5 main.o money.o account.o
+
+main.o: main.cpp money.h account.h
+	g++ -c main.cpp
+
+money.o: money.cpp money.h
+	g++ -c money.cpp
+
+account.o: account.cpp account.h
+	g++ -c account.cpp
+
+clean:
+	rm *.o hw3
